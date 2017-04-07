7599183e2fbbc9ac0eaa39b76b7aca1808fbb4c0
Martin Stoppler <github@ma.stoppe-gp.de>
2017-04-07 15:44:42 +0200

    real version 0.1

--- kandidaten/kandidaten.php
+++ kandidaten/kandidaten.php
@@ -8,7 +8,7 @@ class PT_kandidaten {
     private static $kandidaten;
     private static $kandidatenNW;
     private static $wahlen = array("btw" => "Bundestagswahl");
-    private static $addJS = "";
+    private static $incJS = array();
 
     static function getWk($wahl) {
         if (!in_array($wahl, array_keys(self::$wahlen))) return false;
@@ -59,6 +59,32 @@ class PT_kandidaten {
         return self::$kandidatenNW[$wahl];
     }
 
+    function my_admin_menu() { 
+        add_submenu_page('edit.php?post_type=pt_kandidaten', 'Informationen', 'Informationen', 'manage_options', 'pt_kandidaten_menu', array(__CLASS__, "my_admin_menu_content")); 
+    }
+
+    function my_admin_menu_content() { 
+        if (!current_user_can('manage_options'))
+        {
+          wp_die( __('You do not have sufficient permissions to access this page.') );
+        }
+        echo '<div class="wrap">';
+
+        // header
+
+        echo "<h2>" . __( 'Kandidaten', 'pt_kandidaten' ) . "</h2>";
+?>
+    <p>Dieses Plugin stellt eine tabellarische Übersicht von Kandidaten sowie einen Wahlkreisfindet zur Verfügung.</p>
+<h3>Tabelle</h3>
+<p>Die tabellarische Übersicht kann mit dem Shortcode <pre>[pt-kandidaten-tabelle wahl="btw" sort="wknr" uu="true"]</pre> eingebunden werden. Der Parameter "sort" akzeptiert die Werte "wknr" sowie "name". Der optionale Parameter "uu" aktiviert eine zusätzliche Spalte für Unterstützerformulare.</p>
+<h3>Wahlkreisfinder</h3>
+<p>Der Wahlkreisfinder wird über den Shortcode <pre>[pt-kandidaten-wkf wahl="btw"]</pre> eingebunden.</p> <p>Mit dem zusätzlichen Parameter "start" kann ein bestimmtes Gebiet vorausgewählt werden. <code>[pt-kandidaten-wkf wahl="btw" start="Baden-Württemberg"]</code> deaktiviert beispielsweise die Länderauswahl und zeigt direkt die Untergebiete von Baden-Württemberg an.</p>
+<p>Mit den Parametern "text_nok", "text_uu" sowie "text_nouu" können verschiedene Texte vorgegeben werden. Dabei werden folgende Variablen ersetzt:<ul><li><code>{wknr}</code> – Die Nummer des Wahlkreises</li><li><code>{wkname}</code> – Der Name des Wahlkreises</li><li><code>{kandidat}</code> – Der Name des Kandidaten</li><li><code>{uu}</code> – Der Link zum Unterstützerformular</li></ul></p>
+<?php        
+
+        echo "</div>";
+    }
+
     function create_posttype() {
         register_post_type( 'pt_kandidaten',
             array(
@@ -71,12 +97,34 @@ class PT_kandidaten {
               'show_ui' => true,
               'rewrite' => array('slug' => 'kandidat'),
               'supports' => array('title', 'thumbnail'),
+              'menu_icon'   => 'dashicons-admin-users',
               'register_meta_box_cb' => array(__CLASS__, 'posttype_add_fields')
             )
         );
         add_action( 'save_post_pt_kandidaten', array(__CLASS__, 'posttype_save_data'));
+        add_filter('manage_pt_kandidaten_posts_columns' , array(__CLASS__, 'add_columns'));
+        add_action( 'manage_posts_custom_column' , array(__CLASS__, 'custom_columns'), 10, 2 );
     }
     
+        function add_columns($columns) {
+            return array_merge($columns, 
+                      array('wk' => __('Wahlkreis')));
+        }
+
+    function custom_columns( $column, $post_id ) {
+	    switch ( $column ) {
+		    case 'wk':
+                foreach (array_keys(self::$wahlen) as $wahl) {
+                    $wknames = self::getWk($wahl);
+                    $wknr = get_post_meta( $post_id, '_wk_'.$wahl, true ); 
+                    if ($wknr && ($wknr !== 0)) $wk[] = $wknr." – ".$wknames->$wknr;
+            
+                }
+			    echo implode(", ", $wk);
+			    break;
+	    }
+    }
+
     function posttype_add_fields() {
         foreach (self::$wahlen as $wahl => $wahlname) {
     	    add_meta_box('pt_kandidaten_wk_'.$wahl, 'Wahlkreis '.$wahlname, array(__CLASS__, 'posttype_add_fieldcontent'), 'pt_kandidaten', 'normal', 'high', array("wahl" => $wahl));
@@ -85,7 +133,7 @@ class PT_kandidaten {
     }
     function posttype_add_fieldcontent($post, $args) {
         $wahl = $args['args']['wahl'];
-
+        
         wp_nonce_field( basename( __FILE__ ), "pt_kandidaten_wk_".$wahl."_nonce" );
 
         $curval = get_post_meta($post->ID, '_wk_'.$wahl, true);
@@ -138,13 +186,13 @@ class PT_kandidaten {
 
 
     function incJS() {
+        if (count(self::$incJS) == 0) return;
         $content = "<script type=\"text/javascript\">var wk = []; var orte = []; var kandidaten = [];";
-        foreach (self::$wahlen as $wahl => $wahlname) {
+        foreach (array_keys(self::$incJS) as $wahl) {
             $content .= "\nwk['".$wahl."'] = ".JSON_encode(self::getWk($wahl)).";";
             $content .= "\norte['".$wahl."'] = ".JSON_encode(self::getOrte($wahl)).";";
             $content .= "\nkandidaten['".$wahl."'] = ".JSON_encode(self::getKandidaten($wahl)).";";
         }
-        $content .= "\n".self::$addJS;
         $content .= "</script>";
         echo $content;
         include("js.php");
@@ -153,6 +201,7 @@ class PT_kandidaten {
     function shortcodeWKF($atts) {
         reset(self::$wahlen);
         if (isset($atts['wahl']) && in_array($atts['wahl'], array_keys(self::$wahlen))) $wahl = $atts['wahl']; else $wahl = key(self::$wahlen);
+        self::$incJS[$wahl] = true;
         if (isset($atts['start'])) $start = $atts['start']; else $start = "";
         if (isset($atts['text_nok'])) $text_nok = $atts['text_nok']; else $text_nok = "Im Wahlkreis <strong>{wknr} – {wkname}</strong> wurde leider noch kein Kandidat aufgestellt.";
         if (isset($atts['text_nouu'])) $text_nouu = $atts['text_nouu']; else $text_nouu = "Im Wahlkreis <strong>{wknr} – {wkname}</strong> kandidiert <strong>{kandidat}</strong>. Es sind aber leider noch keine Unterstützerformulare verfügbar.";
@@ -161,9 +210,55 @@ class PT_kandidaten {
         return $content;
     }
 
+    function shortcodeTabelle($atts) {
+        reset(self::$wahlen);
+        if (isset($atts['wahl']) && in_array($atts['wahl'], array_keys(self::$wahlen))) $wahl = $atts['wahl']; else $wahl = key(self::$wahlen);
+        $wknames = self::getWk($wahl);
+
+        $content = "<table><tr><th>Wahlkreis</th><th>Kandidat</th>";   
+        if (isset($atts['uu'])) $content .= "<th></th>";
+        $content .= "</tr>";
+
+        $kandidaten = array_values(self::getKandidaten($wahl));
+
+        if (isset($atts['sort'])) {        
+            switch ($atts['sort']) {
+                case 'name':
+                    foreach ($kandidaten as $k) {
+                        $sar[] = $k['name'];
+                    }
+                    break;
+                case 'wknr':
+                default:
+                    $sar = array_keys(self::getKandidaten($wahl));
+            }
+        } else {
+            $sar = array_keys(self::getKandidaten($wahl));
+        }
+        array_multisort($sar, $kandidaten);
+        foreach ($kandidaten as $kandidat) {
+            $wk = $kandidat['wk'][$wahl];
+            $wkname = $wknames->$wk;
+        $content .= "<tr>
+<td>{$wk} – <strong>{$wkname}</strong></td>
+<td>{$kandidat['name']}</td>";
+            if (isset($atts['uu'])) {
+                if (isset($kandidat['uu'][$wahl])) $content .= "<td><a href=\"".esc_attr($kandidat['uu'][$wahl])."\">Unterstützerformular für $wkname</a></td>";
+                else $content .= "<td><small>Noch kein Formular verfügbar</small></td>";
+            }
+            $content .= "</tr>";
+        }
+        $content .= "</table>";
+
+        return $content;
+    }
+
 }
 
 add_action( 'init', array('PT_kandidaten', 'create_posttype') );
 add_action( 'wp_footer', array('PT_kandidaten', 'incJS') );
 add_shortcode( "pt-kandidaten-wkf", array("PT_kandidaten", "shortcodeWKF"));
+add_shortcode( "pt-kandidaten-tabelle", array("PT_kandidaten", "shortcodeTabelle"));
+add_action('admin_menu', array("PT_kandidaten", 'my_admin_menu')); 
+
 ?>
